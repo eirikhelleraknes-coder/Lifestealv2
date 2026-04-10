79d26fb95666aa79bb243f29ae90f4da7a551cf6
eirik <eirikhelleraknes@gmail.com>
2026-04-10 12:29:15 +0200

    chore: bootstrap Fabric mod project

---
+++ src/client/java/com/lifesteal/client/LifestealClient.java
@@ -0,0 +1,10 @@
+package com.lifesteal.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LifestealClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
